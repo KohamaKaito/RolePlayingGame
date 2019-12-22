5b1bd8d811fedcfbd01787d8c31279117997ef70
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2019-12-22 20:40:10 +0900

    Step3

--- src/main/java/jp/ac/uryukyu/ie/e195746/LivingThing.java
+++ src/main/java/jp/ac/uryukyu/ie/e195746/LivingThing.java
@@ -1,12 +1,30 @@
 package jp.ac.uryukyu.ie.e195746;
 
+/**
+ * LivingThingクラス。
+ *
+ *  String name;　   //名前
+ *  int hitPoint;    //HP
+ *  int attack;      //攻撃力
+ *  boolean dead;    //生死状態（true＝死亡)
+ *
+ * @author kohamakaito
+ * Created by e195746 on 2019/12/22.
+ */
+
 public class LivingThing {
     private String name;
     private int hitPoint;
     private int attack;
     private boolean dead;
 
-
+    /**
+     *コンストラクタ
+     *
+     * @param name 名前
+     * @param maximumHP 最大HP
+     * @param attack 攻撃力
+     */
     public LivingThing (String name, int maximumHP, int attack) {
         this.name = name;
         hitPoint = maximumHP;
@@ -47,6 +65,12 @@ public class LivingThing {
         this.dead = dead;
     }
 
+    /**
+     * 指定した相手に攻撃をするメソッド
+     * 与えるダメージを乱数で指定してwoundedメソッドにダメージの数値を渡す。
+     *
+     * @param opponent 攻撃する相手
+     */
     public void attack(LivingThing opponent){
         if(isDead() != true){
             int damage = (int)(Math.random() * attack);
@@ -55,6 +79,11 @@ public class LivingThing {
         }
     }
 
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
